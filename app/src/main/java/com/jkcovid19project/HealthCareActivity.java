799ede0712414dee83357e93f382a953208cffc5
anandhamurthy <anandhamurthy2000@gmail.com>
2020-06-30 14:10:35 +0530

    Initial commit

---
+++ app/src/main/java/com/jkcovid19project/HealthCareActivity.java
@@ -0,0 +1,64 @@
+package com.jkcovid19project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import com.jkcovid19project.Adapter.GridAdapter;
+
+public class HealthCareActivity extends AppCompatActivity {
+
+
+    private GridView Grid_View;
+
+    String[] web = {
+            "CCC"
+
+    };
+    int[] imageId = {
+            R.drawable.healthcarecenters,
+
+    };
+
+    private ImageView Back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_care);
+        Back = findViewById(R.id.toolbar_icon);
+
+        Grid_View = findViewById(R.id.grid_view);
+        GridAdapter adapter = new GridAdapter(HealthCareActivity.this, web, imageId);
+        Grid_View.setAdapter(adapter);
+
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        Grid_View.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                switch (position) {
+                    case 0:
+                        Intent intent0 = new Intent(HealthCareActivity.this, HealthCareListActivity.class);
+                        intent0.putExtra("type", "data");
+                        startActivity(intent0);
+                        break;
+
+                }
+
+            }
+        });
+
+
+    }
+}
